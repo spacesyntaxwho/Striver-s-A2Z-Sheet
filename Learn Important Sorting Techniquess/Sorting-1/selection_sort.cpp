d9b81410ec8b99650322a4732b462a3837530aaa
Anuj Sanjay Ghadge <spacesyntax37@gmail.com>
2026-07-24 22:34:19 +0530

    Solution added
    Implement selection sort algorithm and main function for user input.

--- Learn Important Sorting Techniquess/Sorting-1/selection_sort.cpp
+++ Learn Important Sorting Techniquess/Sorting-1/selection_sort.cpp
@@ -1,4 +1,28 @@
-class Solution {
-public:
-    
-};
+#include <iostream>
+using namespace std;
+
+void selection_sort(int arr[],int n){
+    for(int i = 0; i<=n-2;i++){
+        int mini = i;
+        for(int j = i;j<=n-1;j++){
+            if (arr[j]<arr[mini]){
+                mini = j;
+            }
+        }
+        swap(arr[i],arr[mini]);
+    }
+}
+int main(){
+    int n;
+    cout<<"Enter the size of array:"<<endl;
+    cin>>n;
+    int arr[n];
+    for(int i = 0; i<n;i++) cin>>arr[i];
+    selection_sort(arr,n);
+    cout<<"Sorted array is:"<<endl;
+    for(int i = 0; i<n;i++) {
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+
+}
